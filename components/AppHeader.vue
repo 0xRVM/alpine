4700dfbf90a2e4414204d2dcdbdac78f087cd982
0xRVM <121198349+0xRVM@users.noreply.github.com>
2024-09-16 17:16:26 -0300

    Update AppHeader.vue

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -5,27 +5,11 @@ const show = ref(false)
 </script>
 
 <template>
-  <header :class="alpine.header.position || 'left'">
-    <div class="menu">
-    </div>
-
-    <div class="overlay" :class="[show && 'show']">
-      <MainNav @link-click="show = !show" />
-    </div>
-
-<div class="logo" style="display: block !important;">
-<img src="/primatelogo-blanco.png" alt="Logo Primate Blanco" width="120" height="60" style="display: block;" />
-
-</div>
-
-
-
-    <div class="main-nav">
-      <div class="main-nav">
-  <!-- MainNav removido temporalmente -->
-</div>
-
-      <MainNav />
+  <header :class="alpine.header.position || 'left'" style="display: flex; justify-content: center;">
+    <div class="logo">
+      <NuxtLink to="/">
+        <img src="/articles/primatelogo-blanco.png" alt="Logo Primate Blanco" width="120" height="60" style="display: block;" />
+      </NuxtLink>
     </div>
   </header>
 </template>
@@ -35,25 +19,10 @@ css({
   header: {
     '--header-padding': '{space.16}',
     '--logo-height': '{space.8}',
-    position: 'relative',
     display: 'grid',
-    gridTemplateColumns: 'repeat(12, minmax(0, 1fr))',
     alignItems: 'center',
     gap: '{space.4}',
     py: 'var(--header-padding)',
-    '.menu': {
-      position: 'absolute',
-      display: 'flex',
-      ':hover': {
-        color: '{color.primary.500}',
-      },
-      '.left &&': {
-        right: 0
-      },
-      '@sm': {
-        display: 'none'
-      }
-    },
     '.logo': {
       display: 'flex',
       justifyContent: 'center',
@@ -61,17 +30,8 @@ css({
       a: {
         display: 'flex',
       },
-    },
-    '.main-nav': {
-      display: 'none',
-      '@sm': {
-        display: 'flex',
-        gridColumn: 'span 8 / span 8',
-        fontSize: '{text.xl.fontSize}',
-        lineHeight: '{text.xl.lineHeight}',
-        fontWeight: '{fontWeight.medium}',
-      }
-    },
+    }
   }
 })
 </style>
+
