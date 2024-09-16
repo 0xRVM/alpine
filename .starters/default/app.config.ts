5c98bd4f9d9b33ef6e3ce6e29f71eb152ec4300d
0xRVM <121198349+0xRVM@users.noreply.github.com>
2024-09-16 18:45:21 -0300

    Update app.config.ts

--- .starters/default/app.config.ts
+++ .starters/default/app.config.ts
@@ -1,31 +1,42 @@
+// https://github.com/nuxt-themes/alpine/blob/main/nuxt.schema.ts
 export default defineAppConfig({
   alpine: {
-    title: 'Primate',
-    description: 'Reflexiones sobre Esports y la evolución de Primate',
+    title: 'Alpine',
+    description: 'The minimalist blog theme',
     image: {
-      src: '/primatelogo-blanco.png',
-      alt: 'Primate Logo',
+      src: '/social-card-preview.png',
+      alt: 'An image showcasing my project.',
       width: 400,
       height: 300
     },
     header: {
-      position: 'right',
+      position: 'right', // possible value are : | 'left' | 'center' | 'right'
       logo: {
-        path: '/primatelogo-blanco.png',
-        pathDark: '/primatelogo-negro.png',
-        alt: 'Primate Logo'
+        path: '/logo.svg', // path of the logo
+        pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
+        alt: 'alpine' // alt of the logo
       }
     },
     footer: {
       credits: {
-        enabled: true,
-        repository: 'https://www.github.com/tu-repo/primate'
+        enabled: true, // possible value are : true | false
+        repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
       },
-      navigation: true,
-      alignment: 'center',
-      message: 'Reflexiones sobre la evolución de Primate'
+      navigation: true, // possible value are : true | false
+      alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
+      message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
+    },
+    socials: {
+      twitter: 'nuxtlabs',
+      instagram: 'atinuxt',
+      linkedin: {
+        icon: 'uil:linkedin',
+        label: 'LinkedIn',
+        href: 'https://www.linkedin.com/company/nuxtlabs'
+      }
+    },
+    form: {
+      successMessage: 'Message sent. Thank you!'
     }
   }
 })
-
-
