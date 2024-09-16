6871bba1ed7c64b6a3b02f04c4bf83c1ef6659b0
0xRVM <121198349+0xRVM@users.noreply.github.com>
2024-09-16 18:39:23 -0300

    Update app.config.ts

--- .starters/default/app.config.ts
+++ .starters/default/app.config.ts
@@ -3,40 +3,29 @@ export default defineAppConfig({
     title: 'Primate',
     description: 'Reflexiones sobre Esports y la evolución de Primate',
     image: {
-      src: '/primatelogo-blanco.png',  // Ruta de tu logo blanco
+      src: '/primatelogo-blanco.png',
       alt: 'Primate Logo',
       width: 400,
       height: 300
     },
     header: {
-      position: 'right',  // posible valor: 'left' | 'center' | 'right'
+      position: 'right',
       logo: {
-        path: '/primatelogo-blanco.png',  // Ruta del logo blanco
-        pathDark: '/primatelogo-negro.png',  // Ruta del logo en modo oscuro
-        alt: 'Primate Logo'  // Texto alternativo para el logo
+        path: '/primatelogo-blanco.png',
+        pathDark: '/primatelogo-negro.png',
+        alt: 'Primate Logo'
       }
     },
     footer: {
       credits: {
-        enabled: true,  // mostrar créditos
-        repository: 'https://www.github.com/tu-repo/primate'  // Ruta de tu repositorio
+        enabled: true,
+        repository: 'https://www.github.com/tu-repo/primate'
       },
-      navigation: true,  // Mostrar la navegación en el footer
-      alignment: 'center',  // Alineación del footer: 'none' | 'left' | 'center' | 'right'
-      message: 'Reflexiones sobre la evolución de Primate'  // Mensaje en el footer
-    },
-    socials: {
-      twitter: 'https://x.com/primateclan',  // Puedes añadir tus redes sociales o dejar en blanco
-      instagram: 'https://www.instagram.com/primateclan/',
-      linkedin: {
-        icon: 'uil:linkedin',
-        label: 'LinkedIn',
-        href: 'https://www.linkedin.com/company/primateclan'  // Ruta de LinkedIn
-      }
-    },
-    form: {
-      successMessage: 'Mensaje enviado. ¡Gracias!'  // Mensaje al enviar formulario
+      navigation: true,
+      alignment: 'center',
+      message: 'Reflexiones sobre la evolución de Primate'
     }
   }
 })
+
 
