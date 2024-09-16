32e1ea4212db7e603590bdf61fd695e2e21f6646
0xRVM <121198349+0xRVM@users.noreply.github.com>
2024-09-16 17:38:59 -0300

    Update AppHeader.vue

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,37 +1,21 @@
-<script lang="ts" setup>
-const alpine = useAppConfig().alpine
-
-const show = ref(false)
-</script>
-
 <template>
-  <header :class="alpine.header.position || 'left'" style="display: flex; justify-content: center;">
+  <header>
     <div class="logo">
-      <NuxtLink to="/">
-        <img src="/articles/primatelogo-blanco.png" alt="Logo Primate Blanco" width="120" height="60" style="display: block;" />
-      </NuxtLink>
+      <img src="/primatelogo-blanco.png" alt="Logo Primate" width="120" height="60" />
     </div>
   </header>
 </template>
 
-<style scoped lang="ts">
-css({
-  header: {
-    '--header-padding': '{space.16}',
-    '--logo-height': '{space.8}',
-    display: 'grid',
-    alignItems: 'center',
-    gap: '{space.4}',
-    py: 'var(--header-padding)',
-    '.logo': {
-      display: 'flex',
-      justifyContent: 'center',
-      height: 'var(--logo-height)',
-      a: {
-        display: 'flex',
-      },
-    }
-  }
-})
-</style>
+<style scoped>
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px; /* Ajusta esto si es necesario */
+  background-color: #000; /* Ajusta el color si es necesario */
+}
 
+.logo img {
+  display: block;
+}
+</style>
